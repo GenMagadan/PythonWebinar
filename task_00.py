30a5e907342a3b557df00498801e649ffd2bcb60
Gennady <genchik111@yandex.ru>
2023-05-22 09:37:49 +0300

    Add task00

---
+++ task_00.py
@@ -0,0 +1,11 @@
+# Вводятся два числа
+a = int(input('Введите первое число a: '))
+b = int(input('Введите первое число b: '))
+
+
+if a > b:
+    print('a больше b')
+elif b > a:
+    print('b больше a')
+else:
+    print('a равно b')
